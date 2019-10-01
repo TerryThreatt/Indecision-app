69bb7b9902c24903630f89fe8d628d72ae0d89ea
Terry Threatt <terry.threatt@gmail.com>
2019-09-30 21:17:35 -0500

    update

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import Option from './Option'
 
-const Options = (props) => { 
-    return ( 
+const Options = (props) => ( 
       <div>
         <button onClick={props.handleDeleteOptions}>Delete All</button>
         {props.options.length === 0 && <p>Please add an option to get started!</p>}
@@ -17,6 +16,4 @@ const Options = (props) => {
         }
       </div>
     )
-  }
-
 export default Options 
